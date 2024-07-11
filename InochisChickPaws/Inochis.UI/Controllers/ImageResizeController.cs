6f9b7991c664dbe31e0eb7f3ceebd607cbcb4603
PoyrazKaradogan <hfk.1998@hotmail.com>
2024-07-11 18:57:54 +0300

    son

---
+++ InochisChickPaws/Inochis.UI/Controllers/ImageResizeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace Inochis.UI.Controllers
+{
+    public class ImageResizeController : Controller
+    {
+       
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public ImageResizeController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        public IActionResult ImageResize()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ImageResize(IFormFile file)
+        {
+            int width = 400;
+            int height = 400;
+            string path = Path.Combine(_hostingEnvironment.WebRootPath, "images", "resized.jpg");
+            Image image = Image.FromStream(file.OpenReadStream(), true, true);
+            var newImage = new Bitmap(width, height);
+            using (var a = Graphics.FromImage(newImage))
+            {
+                a.DrawImage(image, 0, 0, width, height);
+                newImage.Save(path);
+            }
+
+
+                return View();
+        }
+    }
+}
